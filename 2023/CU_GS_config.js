24880a43f68749f6b35667792374a772e02a644f
Flip86gto <75633532+Flip86gto@users.noreply.github.com>
2023-03-05 15:01:22 -0600

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023ilpe",
+      "defaultValue": "2023orore",
       "required": "true",
       "disabled": "true"
     },
